c6a78bb38bab56c768eca993a8b23f2e7edc4a88
James Droste <james@droste.im>
2017-10-01 14:31:31 +1100

    summernote: bump the config to use new options

--- webroot/js/summernote.config.js
+++ webroot/js/summernote.config.js
@@ -17,6 +17,13 @@ window.SUMMERNOTE_CLEANER_CONFIG = {
 	newline: '<br>',
 	notStyle: 'position:absolute;top:0;left:0;right:0',
 	keepHtml: true,
+	// Whitelist borrowed from https://meta.stackexchange.com/a/135909
+	keepOnlyTags: [
+		'a', 'b', 'blockquote', 'code', 'del', 'dd', 'dl',
+		'em', 'h1', 'h2', 'h3', 'i', 'img', 'li', 'ol', 'p',
+		'pre', 's', 'sup', 'sub', 'strong', 'strike', 'ul', 'br',
+		'hr'
+	],
 	keepClasses: false,
 	badTags: ['style', 'script', 'applet', 'embed', 'noframes', 'noscript', 'html'],
 	badAttributes: ['style', 'start'],
